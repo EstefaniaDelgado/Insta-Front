1576354539dbd20aac27c6dec9da3e1390408e5c
EstefaniaDelgado <teff_64@hotmail.com>
2023-10-16 21:20:27 -0500

    add link repository

--- src/components/SectionFooter.jsx
+++ src/components/SectionFooter.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import styles from './SectionFooter.module.css'
+import { Icon } from '@iconify/react';
 
 const SectionFooter = () => {
   return (
     <footer className={styles.containerFooter}>
-      <p>Todos los derechos reservados a Estefania Delgado H.👩🏻</p>
+      <p >Todos los derechos reservados a Estefania Delgado H. <a href="https://github.com/EstefaniaDelgado/Insta-Front" target='_blank'><Icon icon="line-md:github-loop" style={{fontSize:'35px'}}  /></a></p>
     </footer>
   )
 }
